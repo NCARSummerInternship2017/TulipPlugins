d0723b41deeec5adefd1aa63259c2cfc983dbe77
ananta111 <ananta.thapaliya01@gmail.com>
2017-07-11 13:18:06 -0600

    Update helloworld.cpp

--- plugins/helloworld.cpp
+++ plugins/helloworld.cpp
@@ -1,9 +1,9 @@
-/**
+ /**
  *
  * This file is part of Tulip (www.tulip-software.org)
  *
  * Authors: David Auber and the Tulip development Team
- * from LaBRI, University of Bordeaux, University Corporation 
+ * from LaBRI, University of Bordeaux, University Corporation
  * for Atmospheric Research
  *
  * Tulip is free software; you can redistribute it and/or modify
@@ -18,9 +18,19 @@
  *
  */
 
-#include<fstream>
+#include <fstream>
 #include <algorithm>
+#include <vector>
+
+#include <tulip/TlpTools.h>
+#include <tulip/Graph.h>
+#include <tulip/GlScene.h>
+#include <tulip/GraphIterator.h>
+#include <tulip/BooleanProperty.h>
+#include <tulip/ColorProperty.h>
+
 #include "helloworld.h"
+
 #include "fabric.h"
 #include "ibautils/ib_fabric.h"
 #include "ibautils/ib_parser.h"
@@ -30,118 +40,107 @@ using namespace tlp;
 using namespace std;
 
 /*PLUGIN(ImportInfinibandRoutes)*/
-PLUGIN(HelloWorld)
+PLUGIN(InfinibandAnalysis)
 /**
 static const char * paramHelp[] = {
-  // File to Open
-  HTML_HELP_OPEN() \
-  HTML_HELP_DEF( "type", "pathname")\
+   // File to Open
+   HTML_HELP_OPEN() \
+  HTML_HELP_DEF( "type", "pathname") \
   HTML_HELP_BODY() \
   "Hello World" \
   HTML_HELP_CLOSE()
 };
 **/
-  
-HelloWorld::HelloWorld(tlp::PluginContext* context)
-  : tlp::Algorithm(context)
+
+InfinibandAnalysis::InfinibandAnalysis(tlp::PluginContext* context)
+        : tlp::Algorithm(context)
 {
-  //addInParameter<std::string>("file::filename", paramHelp[0],"");
-    
+    //addInParameter<std::string>("file::filename", paramHelp[0],"");
+
 }
 
 
 
-bool HelloWorld::run()
+
+
+
+
+
+bool InfinibandAnalysis::run()
 {
-  assert(graph);
+    assert(graph);
 
-  static const size_t STEPS = 6;
-  if(pluginProgress)
-  {
-    pluginProgress->showPreview(false);
-    pluginProgress->setComment("Starting to Import Routes");
-    pluginProgress->progress(0, STEPS);
-  }
+    static const size_t STEPS = 4;
+    if(pluginProgress)
+    {
+        pluginProgress->showPreview(false);
+        pluginProgress->setComment("Implementing Dijkstra's algorithm on the nodes...");
+        pluginProgress->progress(0, STEPS);
+    }
 
-  /**
-   * while this does not import
-   * nodes/edges, it imports properties
-   * for an existing fabric
-   */
+    /**
+     * while this does not import
+     * nodes/edges, it imports properties
+     * for an existing fabric
+     */
 
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Counting the nodes");
-    pluginProgress->progress(1, STEPS);
-  }
-
-  /**
-   * Open file to read and import per type
-   */
- 
-
-  if(pluginProgress)
-  {
-    pluginProgress->progress(2, STEPS);
-    pluginProgress->setComment("Parsing Routes.");
-  }
-  
-  
-
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Parsing Routes complete.");
-    pluginProgress->progress(3, STEPS);
-  }
-
-  
-      
-  /**
-   * calculate routes outbound
-   * from every port on the fabric
-   */
-  
-
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Calculating Route oversubscription.");
-    pluginProgress->progress(4, STEPS);
-  }
-
-  
-  tlp::Iterator<node> *itnod = graph->getNodes(); //Nodes Iterator
-  tlp:Iterator<edge> *itedge = graph->getEdges(); //Edges Iterator
-  int n_nodes = 0;
-  int n_edges = 0;
-  
-  
-  while( itnod->hasNext()){
     
-    n_nodes++;
-  }
-  
-  while (itedge->hasNext()){
-    n_edges++;
-  }
-  
-  cout << "Hello World!";
-  cout << "There are " <<n_nodes<< " and" << n_edges << " edges in this graph";
-  
-  
-  
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Show the max min average steps");
-    pluginProgress->progress(5, STEPS);
-  }
+   
+    if(pluginProgress)
+    {
+        pluginProgress->setComment("Running...");
+        pluginProgress->progress(1, STEPS);
+    }
+
+    /**
+     * Open file to read and import per type
+     */
+    
+
+    
+    if(pluginProgress)
+    {
+        pluginProgress->progress(2, STEPS);
+        pluginProgress->setComment("Calculating the number of nodes..");
+    }
+
+   
   
   
 
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Calculating Route oversubscription complete.");
-    pluginProgress->progress(STEPS, STEPS);
-  }
+   
+    tlp::Iterator<tlp::node> *itnodes = graph->getNodes();
+    tlp::Iterator<tlp::edge> *itedges = graph->getEdges();
+    int num_nodes = 0;
+    int num_edges = 0;
 
-  return true;
+    while(itnod->hasNext()){
+      
+        num_nodes++;
+    }
+  
+    while (itedges->hasNext()){
+      num_edges++;
+    }
+  
+    cout <<"Hello World!";
+    cout << "The graph has: \n "<< num_nodes << " number of nodes, and \n" << num_edges <<"number of edges";
+   
+   if(pluginProgress)
+    {
+        pluginProgress->progress(3, STEPS);
+        pluginProgress->setComment("Using BooleanProperty to update viewSelection..");
+    }
+   
+   
+   
+  
+   
+    if(pluginProgress)
+    {
+        pluginProgress->setComment("Dijkstra's Algorithm implemented...");
+        pluginProgress->progress(STEPS, STEPS);
+    }
+
+    return true;
 }

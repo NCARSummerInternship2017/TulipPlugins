28c291760926b99b198041591e69bdaa2d5a67bf
ananta111 <ananta.thapaliya01@gmail.com>
2017-07-11 16:56:10 -0600

    Update realRoutes.h

--- plugins/realRoutes.h
+++ plugins/realRoutes.h
@@ -1,23 +1,6 @@
-/**
- *
- * This file is part of Tulip (www.tulip-software.org)
- *
- * Authors: David Auber and the Tulip development Team
- * from LaBRI, University of Bordeaux, University Corporation
- * for Atmospheric Research
- *
- * Tulip is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * Tulip is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- */
-
+//
+// Created by zliu58 on 7/5/17.
+//
 
 #include <tulip/TulipPluginHeaders.h>
 #include <tulip/StringProperty.h>
@@ -31,23 +14,20 @@
 
 namespace ib = infiniband;
 
-class realRoutes: public tlp::Algorithm{
-    PLUGININFORMATION("Real Routes (Infiniband)",
+class RouteAnalysis_All: public tlp::Algorithm{
+    PLUGININFORMATION("Real Rohte",
                       "zz",
                       "06/27/2017",
                       "print out the real routing path",
                       "alphe",
                       "Infiniband");
 public:
-    realRoutes(tlp::PluginContext* context);
+    RouteAnalysis_All(tlp::PluginContext* context);
 
     bool run();
     unsigned int count_hops(const ib::entity_t * source_entity, const ib::entity_t * target_entity,tlp::Graph * const graph);
     unsigned int help_count(ib::tulip_fabric_t * const fabric, tlp::Graph * const graph,
-                            std::vector<ib::entity_t *> tmp, const ib::entity_t * real_target,
+                            std::vector<ib::entity_t *> &tmp, const ib::entity_t * real_target,
                             ib::lid_t target_lid, tlp::StringProperty *getGuid);
 };
 #endif //TULIPTEST_ROUTEANALYSIS_H
-
-
-

ebb8a8ed46dadcf4a5baed148ca7645ce6cd0d80
ananta111 <ananta.thapaliya01@gmail.com>
2017-07-12 10:35:32 -0600

    Update realRoutes.h

--- plugins/realRoutes.h
+++ plugins/realRoutes.h
@@ -15,7 +15,7 @@
 namespace ib = infiniband;
 
 class RouteAnalysis_All: public tlp::Algorithm{
-    PLUGININFORMATION("Real Routes",
+    PLUGININFORMATION("Count Real Routes - All Nodes",
                       "zz",
                       "06/27/2017",
                       "print out the real routing path",
@@ -25,9 +25,9 @@ public:
     RouteAnalysis_All(tlp::PluginContext* context);
 
     bool run();
-    unsigned int count_hops(const ib::entity_t * source_entity, const ib::entity_t * target_entity,tlp::Graph * const graph);
-    unsigned int help_count(ib::tulip_fabric_t * const fabric, tlp::Graph * const graph,
-                            std::vector<ib::entity_t *> &tmp, const ib::entity_t * real_target,
-                            ib::lid_t target_lid, tlp::StringProperty *getGuid);
+    const ib::entity_t * getMyEntity(const tlp::node node,ib::tulip_fabric_t * const fabric);
+    int count_hops(tlp::node source_node, tlp::node target_node,tlp::Graph * const graph);
+    int help_count(ib::tulip_fabric_t * const fabric, tlp::Graph * const graph,
+                            std::vector<ib::entity_t *> &tmp, const ib::entity_t * real_target);
 };
 #endif //TULIPTEST_ROUTEANALYSIS_H

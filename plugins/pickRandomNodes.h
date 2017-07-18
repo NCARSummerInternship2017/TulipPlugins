32a7dfcf36a111c37d86f8aa846c0b550963f1de
ananta111 <ananta.thapaliya01@gmail.com>
2017-07-18 11:07:33 -0600

    Update pickRandomNodes.h

--- plugins/pickRandomNodes.h
+++ plugins/pickRandomNodes.h
@@ -1,6 +1,22 @@
-//
-// Created by anantat on 7/12/17.
-//**
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux, University Corporation
+ * for Atmospheric Research
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
 
 #pragma once
 
@@ -14,18 +30,16 @@
 class randomNodes: public tlp::Algorithm {
 public:
     PLUGININFORMATION("Picks two random nodes from the graph",
-    "Ananta Thapaliya",
+    "Ananta Thapaliya & Zenzhen Liu",
     "06/16/15",
-    "Picks any two random nodes from the graph for testing purposes.",
+    "Picks two random nodes from the graph for testing purposes.",
     "alpha",
     "Infiniband")
 
+//Constructor
     randomNodes(tlp::PluginContext* context);
 
-    /**
-     * @brief import infiniband routes
-     * @warning currently only works if static data is retained from import
-     */
+//Tulip's Main Function 
     bool run();
 };
 

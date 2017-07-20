b9e9e3f098729709274c0a2d00f7cb3a38bbf564
ananta111 <ananta.thapaliya01@gmail.com>
2017-07-20 15:43:12 -0600

    Update testRandom.h

--- plugins/testRandom.h
+++ plugins/testRandom.h
@@ -1,33 +1,45 @@
-//
-// Created by anantat on 7/12/17.
-//**
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux, University Corporation
+ * for Atmospheric Research
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
 
 #pragma once
 
 #include <tulip/TulipPluginHeaders.h>
 #include <iostream>
 #include <algorithm>
-#include "pickRandomNodes.h"
-#include "realRoutes.h"
 
 #ifndef IB_ROUTES_H
 #define IB_ROUTES_H
 
 class testRandom: public tlp::Algorithm {
 public:
-    PLUGININFORMATION("Stochastic Analysis",
-    "Ananta Thapaliya",
+    PLUGININFORMATION("Performs Random Tests",
+    "Ananta Thapaliya & Zenzhen Liu",
     "06/16/15",
-    "Picks any two random nodes from the graph for testing purposes.",
+    "Picks two random nodes from the graph for testing purposes.",
     "alpha",
     "Infiniband")
 
+//Constructor
     testRandom(tlp::PluginContext* context);
 
-    /**
-     * @brief import infiniband routes
-     * @warning currently only works if static data is retained from import
-     */
+//Tulip's Main Function 
     bool run();
 };
 
